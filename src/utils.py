6aef3e84bc5dfeacd7baebe12547261999e896d2
Tatyana Kavardaeva <solarpoint@yandex.ru>
2024-04-25 23:59:48 +0300

    updated utils.py

--- src/utils.py
+++ src/utils.py
@@ -2,15 +2,20 @@ import json
 from src.class_operation import Operation
 
 
-def get_executed_operations(file):
-    """Получает данные по операциям из json файла и возвращает список словарей выполненныx операций"""
-    with open(file, "r", encoding="utf-8") as f:
-        operations = json.load(f)
-        executed_operations = []
-        for operation in operations:
-            if operation.get('state') == 'EXECUTED':
-                executed_operations.append(operation)
-        return executed_operations
+def get_all_operations(file):
+    """Читает json файл и преобразует в объект"""
+    with open(file, "r", encoding='utf-8') as f:
+        all_operations = json.load(f)
+        return all_operations
+
+
+def get_executed_operations(all_operations):
+    """Возвращает список словарей выполненныx операций"""
+    executed_operations = []
+    for dict_operation in all_operations:
+        if dict_operation.get('state') == 'EXECUTED':
+            executed_operations.append(dict_operation)
+    return executed_operations
 
 
 def sort_operations_by_date(list_operations):
@@ -19,15 +24,23 @@ def sort_operations_by_date(list_operations):
     return sorted_operations
 
 
-def display_operations(sorted_operations, count_operation):
-    """Выводит указанное количество операций из отсортированного списка по шаблону:
+def get_operations(sorted_operations):
+    """Формирует список экземпляров Операций из словарей с данными операций"""
+    list_operations = []
+    for operations in sorted_operations:
+        operation = Operation(operations['date'], operations['description'], operations.get('from'), operations['to'],
+                              operations['operationAmount'])
+        list_operations.append(operation)
+    return list_operations
+
+
+def display_operations(list_operations, count_operation):
+    """Выводит указанное количество операций из списка экземпляров операций по шаблону:
     14.10.2018 Перевод организации
     Visa Platinum 7000 79** **** 6361 -> Счет **9638
     82771.72 USD"""
     result = ''
-    list_operations = sorted_operations[:count_operation]
-    for operations in list_operations:
-        operation = Operation(operations['date'], operations['description'], operations.get('from'), operations['to'],
-                              operations['operationAmount'])
+    list_operations = list_operations[:count_operation]
+    for operation in list_operations:
         result += f"{operation}\n"
     return result
