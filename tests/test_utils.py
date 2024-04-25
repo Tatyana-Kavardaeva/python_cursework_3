73252330bbc37f0dfa8bffe87dbc06abad897c91
Tatyana Kavardaeva <solarpoint@yandex.ru>
2024-04-26 00:01:15 +0300

    updated test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,8 +1,20 @@
-from src.utils import get_executed_operations, sort_operations_by_date, display_operations
+import os
+from src.class_operation import Operation
+from src.utils import get_all_operations, get_executed_operations, sort_operations_by_date, display_operations, get_operations
+
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+data_dir = os.path.join(current_dir, "..", "data")
+file_path = os.path.join(data_dir, "test_operations.json")
+
+
+def test_get_all_operations():
+    assert len(get_all_operations(file_path)) == 3
 
 
 def test_get_executed_operations():
-    assert len(get_executed_operations("C:/Users/User/PycharmProjects/pythonProject15/data/test_operations.json")) == 2
+    all_operations = get_all_operations(file_path)
+    assert len(get_executed_operations(all_operations)) == 2
 
 
 def test_sort_operations_by_date():
@@ -20,39 +32,18 @@ def test_sort_operations_by_date():
 
 
 def test_display_operations():
-    sorted_operations = [
-        {
-            "id": 441945886,
-            "state": "EXECUTED",
-            "date": "2019-08-26T10:50:58.294041",
-            "operationAmount": {
-                "amount": "31957.58",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
-            },
-            "description": "Перевод организации",
-            "from": "Maestro 1596837868705199",
-            "to": "Счет 64686473678894779589"
-        },
-        {
-            "id": 41428829,
-            "state": "EXECUTED",
-            "date": "2019-07-03T18:35:29.512364",
-            "operationAmount": {
-                "amount": "8221.37",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод организации",
-            "from": "MasterCard 7158300734726758",
-            "to": "Счет 35383033474447895560"
-        }]
+    list_operations = [
+        Operation(date='2019-08-26T10:50:58.294041', description='Перевод организации',
+                  where_from='Maestro 1596837868705199', to='Счет 64686473678894779589',
+                  operationamount={'amount': '31957.58',
+                                   'currency': {'name': 'руб.', 'code': 'RUB'}}),
+        Operation(date='2019-07-03T18:35:29.512364', description='Перевод организации',
+                  where_from='MasterCard 7158300734726758', to='Счет 35383033474447895560',
+                  operationamount={'amount': '8221.37',
+                                   'currency': {'name': 'USD', 'code': 'USD'}})
+    ]
     count_operation = 2
-    result = display_operations(sorted_operations, count_operation)
+    result = display_operations(list_operations, count_operation)
     expected_output = (
         "26.08.2019 Перевод организации\n"
         "Maestro 1596 83** **** 5199 -> Счет **9589\n"
